a1c0ba1d6cc61953b280b89890cfcdacea317dc2
Barrett Allen <bear9151@Barretts-iMac.local>
2017-04-15 20:27:41 -0400

    Pushing initial prompt

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 var fs = require('fs'),
     inquirer = require('inquirer'),
     basiccard = require('./basiccard.js'),
-    clozecard = require('./clozecard.js'),
-    firstInput = process.argv[2],
-    secondInput = process.argv[3];
+    clozecard = require('./clozecard.js');
+
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'start',
+        message: 'Which type of card would you like to create?',
+        choices: [
+            'Basic Flash Card(s) - (Front and Back)',
+            'Cloze Flash Card(s) - (Fill in the blank)'
+        ]
+    }
+]).then(function (selection) {
+    if (selection.start === 'Basic Flash Card(s) - (Front and Back)') {
+        console.log('Basic Chosen');
+    } else if (selection.start === 'Cloze Flash Card(s) - (Fill in the blank)') {
+        console.log('Cloze Chosen');
+    } else {
+        console.log("Uhoh! Something went wrong. Try again.");
+    }
+});
