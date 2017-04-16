d46dbbebb18f1aec709eb1de6a4098a0e13af2c5
Barrett Allen <bear9151@Barretts-iMac.local>
2017-04-15 20:57:48 -0400

    basic card on track

--- app.js
+++ app.js
@@ -15,9 +15,24 @@ inquirer.prompt([
     }
 ]).then(function (selection) {
     if (selection.start === 'Basic Flash Card(s) - (Front and Back)') {
-        console.log('Basic Chosen');
+        console.log("Great! Let's make a basic card...");
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'basicFront',
+                message: "Ok, what's the question?"
+            },
+            {
+                type: 'input',
+                name: 'basicBack',
+                message: "Great, now what's the answer?"
+            }
+            ]).then(function(data){
+                var basicCard = new basiccard(data.basicFront, data.basicBack);
+                console.log("Here's your card -> " + basicCard.front + " | " + basicCard.back);
+        })
     } else if (selection.start === 'Cloze Flash Card(s) - (Fill in the blank)') {
-        console.log('Cloze Chosen');
+        console.log("Great! Let's make a cloze-deletion card...");
     } else {
         console.log("Uhoh! Something went wrong. Try again.");
     }
